4cdeec894468e2a4b23f2fbd461b776bbc73917e
Gonza <gonzalogramagia@gmail.com>
2026-01-14 15:30:59 -0300

    style: add padding bottom to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 
 export default function Footer() {
   return (
-    <footer className="mb-16 mt-4 flex flex-col items-center w-full max-w-5xl mx-auto px-4">
+    <footer className="mb-16 mt-4 flex flex-col items-center w-full max-w-5xl mx-auto px-4 pb-8">
       <div className="w-full border-t border-neutral-200 mb-16" />
       <ul className="font-sm flex flex-row flex-wrap gap-4 text-neutral-600">
       </ul>
